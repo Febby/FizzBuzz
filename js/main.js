52178b2119e832e8c082751ed41d9630e8e074b4
Febby <gunawan.febby@gmail.com>
2014-06-30 14:23:39 +0700

    Updated markup and js file

--- js/main.js
+++ js/main.js
@@ -1,21 +1,53 @@
-//variables
-var ul =$("ul")
+$(document).ready(function() {
 
-//Init Function
-function myBuzz(){
-for ( var count = 1; count <= 100; count++) {
-		if (count % 3 === 0 && count % 5 === 0) {
-			ul.append("<li class='fizzbuzz'><span>FizzBuzz!</span></li>");
-		}
-		else if (count % 3 === 0) {
-			ul.append("<li class='fizz'><span>Fizz</span></li>");
-		}
-		else if (count % 5 === 0) {
-			ul.append("<li class='buzz'><span>Buzz</span></li>");
-		}
-		else {
-			ul.append("<li><span>" + count + "</span></li>");
-		}
-	}
-  }
-myBuzz();
+
+
+    $(document).on('click', '#submit', function(e) {
+        //Init var
+        var guessNumber = document.getElementById('userinput').value;
+        var result = document.getElementById("result-list");
+        var ul = $("ul");
+
+        //Check user input
+
+        if (isNaN(parseInt(guessNumber))) {
+            //init variable for error notification
+            var notify = document.createElement('li');
+            notify.innerHTML = "<p>Whoops! Try Again! </p>";
+
+            //Clean result container
+            while (result.hasChildNodes()) {
+                result.removeChild(result.lastChild);
+            }
+
+            result.appendChild(notify);
+            return false;
+        } else {
+
+            while (result.hasChildNodes()) {
+                result.removeChild(result.lastChild);
+            }
+
+            for (var count = 1; count <= guessNumber; count++) {
+                if (count % 3 === 0 && count % 5 === 0) {
+                    ul.append("<li class='animated fadeIn fizzbuzz'><span>FizzBuzz!</span></li>");
+                } else if (count % 3 === 0) {
+                    ul.append("<li class='animated fadeIn fizz'><span>Fizz</span></li>");
+                } else if (count % 5 === 0) {
+                    ul.append("<li class='animated fadeIn buzz'><span>Buzz</span></li>");
+                } else {
+                    ul.append("<li class='animated fadeIn'><span>" + count + "</span></li>");
+                }
+            }
+
+
+        }
+        e.preventDefault();
+    })
+
+
+
+});
+
+
+
